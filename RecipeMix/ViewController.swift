5c878afcdc5dd8afc6b64cefb12984f53ff1af79
andrestc87 <andrestc87@gmail.com>
2020-09-06 16:08:31 -0500

    Initial Commit

---
+++ RecipeMix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeMix
+//
+//  Created by andres tello campos on 9/6/20.
+//  Copyright © 2020 andres tello campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
